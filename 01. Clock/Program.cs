c5850fb3d02840e878ba24a28d818fcd1c0c218e
StefaniaStefanova <StefaniaStefanova@users.noreply.github.com>
2023-12-22 23:27:08 +0200

    Add project files.

---
+++ 01. Clock/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                for (int h = 0; h < 24; h++)
+                {
+                    for (int min = 0; min < 60; min++)
+                    {
+                        Console.WriteLine($"{h}:{min}");
+                    }
+                }
+            
+        }
+    }
+
+}
+    
---
+++ 03. Combinations/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = int.Parse(Console.ReadLine());
+            int numberOfWays = 0;
+
+            for (int x1 = 0; x1 <= result; x1++)
+            {
+                for (int x2 = 0; x2 <= result; x2++)
+                {
+                    int x3 = result - x1 - x2;
+                    if (x3 >= 0)
+                    {
+                        numberOfWays++;
+                    }
+                }
+            }
+
+            Console.WriteLine(numberOfWays);
+        }
+    }
+}
